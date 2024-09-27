e1caf81b426705fea915807f9760952732913c11
Jose Luis Duran <jlduran@users.noreply.github.com>
2024-09-27 05:45:50 +0000

    Make 'garu' a function
    Using the current branch as the reference for now.

--- git-arc.plugin.zsh
+++ git-arc.plugin.zsh
@@ -6,7 +6,6 @@ alias garc='git arc create'
 alias garcl='git arc create -l'
 alias garl='git arc list'
 alias gars='git arc stage'
-alias garu='git arc update'
 
 #
 # Functions
@@ -15,4 +14,9 @@ alias garu='git arc update'
 function garlm() {
   local b="$(git_current_branch)"
   git arc list main.."$b"
+}
+
+function garu() {
+  local b="$(git_current_branch)"
+  git arc update "$b"
 }
